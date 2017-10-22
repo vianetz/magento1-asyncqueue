ae6fba7ae1cd59c7b951e68b135a7bead364b9e0
Christoph Massmann <c.massmann@vianetz.com>
2017-10-22 17:04:08 +0200

    updated license and header files

--- src/app/code/community/Vianetz/AsyncQueue/Model/QueueInterface.php
+++ src/app/code/community/Vianetz/AsyncQueue/Model/QueueInterface.php
@@ -4,19 +4,18 @@
  *
  * @section LICENSE
  * This file is created by vianetz <info@vianetz.com>.
- * The Magento module is distributed under a commercial license.
- * Any redistribution, copy or direct modification is explicitly not allowed.
+ * The code is distributed under the GPL license.
  *
  * If you did not receive a copy of the license and are unable to
  * obtain it through the world-wide-web, please send an email
  * to license@vianetz.com so we can send you a copy immediately.
  *
  * @category    Vianetz
- * @package     Vianetz_AsyncQueue
+ * @package     Vianetz\AsyncQueue
  * @author      Christoph Massmann, <C.Massmann@vianetz.com>
  * @link        http://www.vianetz.com
- * @copyright   Copyright (c) since 2006 vianetz - C. Massmann (http://www.vianetz.com)
- * @license     http://www.gnu.org/licenses/gpl-2.0.txt GNU GENERAL PUBLIC LICENSE
+ * @copyright   Copyright (c) since 2006 vianetz - Dipl.-Ing. C. Massmann (http://www.vianetz.com)
+ * @license     http://www.gnu.org/licenses/gpl-3.0.txt GNU GENERAL PUBLIC LICENSE
  */
 
 interface Vianetz_AsyncQueue_Model_QueueInterface
